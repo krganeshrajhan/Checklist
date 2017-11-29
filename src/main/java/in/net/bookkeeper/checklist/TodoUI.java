30bf2cf7e8be0a89118ccbc51f64b555c94cb6ec
krganeshrajhan <krganeshrajhan@gmail.com>
2017-11-29 23:28:33 +0800

    Checklist

---
+++ src/main/java/in/net/bookkeeper/checklist/TodoUI.java
@@ -0,0 +1,26 @@
+package in.net.bookkeeper.checklist;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * Created by krganeshrajhan on 29/11/17.
+ */
+@SpringUI
+public class TodoUI extends UI {
+
+
+    private VerticalLayout rootLayout;
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+        rootLayout = new VerticalLayout();
+        setContent(rootLayout);
+        rootLayout.addComponent(new Label("Checklist"));
+    }
+}
+
