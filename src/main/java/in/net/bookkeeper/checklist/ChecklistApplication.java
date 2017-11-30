d38783f1d13af5da895521ba8466adddbdd10d90
krganeshrajhan <krganeshrajhan@gmail.com>
2017-11-30 16:03:36 +0800

    Add a index file

--- src/main/java/in/net/bookkeeper/checklist/ChecklistApplication.java
+++ src/main/java/in/net/bookkeeper/checklist/ChecklistApplication.java
@@ -2,9 +2,16 @@ package in.net.bookkeeper.checklist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChecklistApplication {
+public class ChecklistApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ChecklistApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChecklistApplication.class, args);
